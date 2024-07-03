d37a9842802627bde5d3c2a6e25fea9c67ce7a4f
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2024-07-03 15:09:25 +0200

    test templates

---
+++ upcoming/bookworm/amd64/eth-node-prysm/5.0.4-1/src/debian/hack/workspace_status.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# action_env through bazel does not work https://github.com/bazelbuild/bazel/issues/8578
+# providing env variables also does not work
+# patching each update is not feasible on each update, neither automatable
+
+echo STABLE_GIT_COMMIT "3b184f43c86baf6c36478f65a5113e7cf0836d41"
+echo DATE "2024-07-03 15:08:47+00:00"
+echo DATE_UNIX "1720012127"
+echo DOCKER_TAG "v5.0.4"
+echo STABLE_GIT_TAG "v5.0.4"
